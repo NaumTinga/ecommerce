bce11ff2c594b4bd9e22eaa3b3aec06a48dab63e
NaumTinga <53570550+NaumTinga@users.noreply.github.com>
2023-07-11 15:18:23 +0200

    Axios to fetch data from backend and code Identation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import { Container } from 'react-bootstrap';
+import { Container } from "react-bootstrap";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    
     <BrowserRouter>
-    <Header />
-    <main className="py-3">
-      <Container>
-        <Routes>
-          {" "}
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/product/:id" element={<ProductScreen />} />
-        </Routes>
-      </Container>
-    </main>
-    <Footer />
-  </BrowserRouter>
-    
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            {" "}
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,34 @@
-import React from 'react';
-
-import { Row, Col } from 'react-bootstrap';
-import products from '../products';
-import Product from '../components/Product';
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import products from "../products";
+import Product from "../components/Product";
+import axios from "axios";
 
 function HomeScreen() {
+  const [products, setProducts] = useState([]); //to update data loaded
+
+  //to load data from axios(backend)
+  useEffect(() => {
+    async function fetchProducts() {
+      const { data } = await axios.get("/api/products/");
+      setProducts(data);
+    }
+    
+    fetchProducts()
+  }, []);
+
   return (
-    <div><h1>Latest Products</h1>
-    <Row>
-        {products.map(product => (
-            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                <Product product={product}/>
-            </Col>
+    <div>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
         ))}
-    </Row>
+      </Row>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
